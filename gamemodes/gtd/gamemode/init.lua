91d0be6641470b7bd1f3225e9d18998bf1a31c7e
vortexan7 <65450946+vortexan7@users.noreply.github.com>
2020-05-31 01:52:50 +0100

    ui - hp bar

--- gamemodes/gtd/gamemode/init.lua
+++ gamemodes/gtd/gamemode/init.lua
@@ -1,6 +1,10 @@
 AddCSLuaFile("shared.lua")
 include("shared.lua")
 
+resource.AddFile("content/materials/background.png")
+resource.AddFile("content/materials/hp.png")
+resource.AddFile("content/materials/armor.png")
+
 function GM:PlayerSpawn(ply)
     if not ply:HasFirstSpawned() then
         player_manager.SetPlayerClass( ply, "player_td")
