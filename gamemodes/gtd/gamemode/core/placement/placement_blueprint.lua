976095be9ce37a249ebb600d235f0e48a8b3faa5
OfficialCee <tommyc6475@hotmail.com>
2020-06-01 08:19:01 +0100

    Update placement_blueprint.lua

--- gamemodes/gtd/gamemode/core/placement/placement_blueprint.lua
+++ gamemodes/gtd/gamemode/core/placement/placement_blueprint.lua
@@ -1,2 +1,14 @@
 
 -- All placeables will derive from here, using a baseclass as ...a base lol
+PlacementSystem = PlacementSystem or {}
+
+-- Tower preview and where he'd like to place the tower.
+function PlacementSystem.CreateBlueprint( localPlayer, towerIndex )
+
+
+end
+
+-- CreateTower would be executed when a player is able to place the tower.
+function PlacementSystem.CreateTower( ownerPlayer, towerIndex ) -- serverside
+		
+end
